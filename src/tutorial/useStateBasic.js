e03e136d5271845bed5e52467aaf59399e4b0205
unknown <saransh.tyagi@soprasteria.com>
2021-08-10 18:35:01 +0530

    advance react

---
+++ src/tutorial/useStateBasic.js
@@ -0,0 +1,10 @@
+import React , {useState} from 'react';
+
+function UseStateBasic() {
+    console.log(useState("Saransh"));
+    return (
+        <div></div>
+    )
+}
+
+export default UseStateBasic
