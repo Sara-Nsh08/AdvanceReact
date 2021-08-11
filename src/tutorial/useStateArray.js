e8b519183be278c1d524cff8b5d49a569db660b2
unknown <saransh.tyagi@soprasteria.com>
2021-08-11 10:15:35 +0530

    advance react

--- src/tutorial/useStateArray.js
+++ src/tutorial/useStateArray.js
@@ -2,6 +2,10 @@ import React from 'react';
 import {data} from "./Data";
 function UseStateArray() {
     const [people, setPeople]=React.useState(data)
+    const removeItem=(id)=>{
+        let newPeople=people.filter((person)=>person.id !== id);
+        setPeople(newPeople);
+    }
     return (
         <>
         {
@@ -10,6 +14,7 @@ function UseStateArray() {
             return(
                 <div key={id} className='item'>
                     <h4>{name}</h4>
+                    <button onClick={()=> removeItem(id)}>Remove Him</button>
                 </div>
             )
         }
