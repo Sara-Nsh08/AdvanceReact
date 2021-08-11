05a099591a7194a688d61175c341174f2b1d9f67
unknown <saransh.tyagi@soprasteria.com>
2021-08-11 11:21:16 +0530

    advance react

--- src/tutorial/useStateObject.js
+++ src/tutorial/useStateObject.js
@@ -5,17 +5,20 @@ function UseStateObject() {
         name: "Thanos",
         age: 42,
         message: "What did it cost?",
-
     });
+    const[name, setName]= useState("tony Stark")
+    const[age, setAge]= useState(32)
+    const[message, setMessage]= useState("I am Iron Man")
     const changeMesaage = () => {
         setPerson({ ...person, message: "Everything!!" });
+        setMessage("OKay!!");
     }
     return (
         <div>
             <>
-                <h3>{person.name}</h3>
-                <h3>{person.age}</h3>
-                <h3>{person.message}</h3>
+                <h3>{name}</h3>
+                <h3>{age}</h3>
+                <h3>{message}</h3>
                 <button className="btn" onClick={changeMesaage}>Change my message</button>
 
             </>
