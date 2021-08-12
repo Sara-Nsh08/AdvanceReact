5568a98a5b3a0e27a7440f3a6f10245a39705d4d
unknown <saransh.tyagi@soprasteria.com>
2021-08-12 10:12:59 +0530

    Started and created jason server

--- src/tutorial/LearnJSON.js
+++ src/tutorial/LearnJSON.js
@@ -2,14 +2,14 @@ import React from 'react';
 import axios from 'axios';
 
 function LearnJSON() {
-    const handleClick=(b)=> {
-        const promis=axios.post("http://localhost:3000/")
-    }
+    //const handleClick=(b)=> {
+       // const promis=axios.post("http://localhost:3000/")
+    //}
     return (
         <>
-            <h1>hello</h1>
-            <button type="button" className='btn' onClick={handleClick}>Send User</button>
-            <button type="button" className='btn' onClick={getUsers}>Get User</button>
+            <h1>hello we are learning json</h1>
+           {/*<button type="button" className='btn' onClick={handleClick}>Send User</button>
+            <button type="button" className='btn' onClick={getUsers}>Get User</button>*/}
         </>
     )
 }
