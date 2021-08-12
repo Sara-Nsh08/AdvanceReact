874847d3172fb055c8c35ef0ef6bbcf8e9833501
unknown <saransh.tyagi@soprasteria.com>
2021-08-12 10:49:27 +0530

    Star posting to json-server

--- src/tutorial/LearnJSON.js
+++ src/tutorial/LearnJSON.js
@@ -2,14 +2,29 @@ import React from 'react';
 import axios from 'axios';
 
 function LearnJSON() {
-    //const handleClick=(b)=> {
-       // const promis=axios.post("http://localhost:3000/")
-    //}
+    const handleClick = (b) => {
+        const promis = axios.post("http://localhost:4000/users", { //setting the port code
+            name: "Sara"
+        }, {
+            headers: {
+                ['content-type']: 'application/json' //setting the server type
+            }
+        })
+        promis.then(response => {
+            console.log(response)  //console loging response from server
+        })
+        promis.catch(error => {
+            console.error(Error)    //console logging error from server if any
+        })
+        let c=b;
+        console.log(c);
+    }
+
     return (
         <>
             <h1>hello we are learning json</h1>
-           {/*<button type="button" className='btn' onClick={handleClick}>Send User</button>
-            <button type="button" className='btn' onClick={getUsers}>Get User</button>*/}
+            <button type="button" className='btn' onClick={handleClick}>Post User</button>
+            {/*<button type="button" className='btn' onClick={getUsers}>Get User</button>*/}
         </>
     )
 }
