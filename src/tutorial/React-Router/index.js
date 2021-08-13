d8f1dc3f2ffa4c4a3b5a9eebcaab907da283a2e4
unknown <saransh.tyagi@soprasteria.com>
2021-08-13 10:36:42 +0530

    react-router-dom functioning with navigation bar

--- src/tutorial/React-Router/index.js
+++ src/tutorial/React-Router/index.js
@@ -7,32 +7,26 @@ import People from './people';
 import Person from './person';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-
 function ReactRouterDom() {
     return (
         <Router>
-            <Route path="/">
-                <Home />
-            </Route>
-            <Route path="/about">
-                <About />
-            </Route>
-            <Route path="/error">
-                <Error />
-            </Route>
-            <Route path="/people">
-                <People />
-            </Route>
-            <Route path="/person">
-                <Person />
-            </Route>
-            <Route path="/navbar">
-                <Navbar/>
-            </Route>
+            <Navbar />
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route path="/about">
+                    <About />
+                </Route>
+                <Route path="/people">
+                    <People />
+                </Route>
+                <Route path="*">
+                    <Error />
+                </Route>
+
+            </Switch>
         </Router>
-
-
     )
 }
-
 export default ReactRouterDom
--- src/tutorial/React-Router/people.js
+++ src/tutorial/React-Router/people.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { data } from '../Data';
 
 function People() {
+    const [people, setPeople] = useState(data);
     return (
         <div>
-            
+            <h1>People page</h1>
+            {
+                people.map((person) => {
+                    return (
+                        <div key={person.id} className="item">
+                            <h4>{person.name}</h4>
+                        </div>
+                    );
+                })}
         </div>
     )
 }
-
 export default People
