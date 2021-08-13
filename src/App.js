c16455bf0fbb76d74f0157a0cbad49377c5b8f00
unknown <saransh.tyagi@soprasteria.com>
2021-08-13 13:29:23 +0530

    learned ContextAPI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./index.css";
-import ReactRouterDom from "./tutorial/React-Router/index";
+import ContextAPI from "./tutorial/Context";
+//import PropDrilling from "./tutorial/propDrilling";
+//import ReactRouterDom from "./tutorial/React-Router/index";
 
 function App() {
   return (
     <div className="container">
-      <ReactRouterDom />
+      <ContextAPI />
     </div>
   )
 }
